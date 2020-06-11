be2fd64c58e4d8fa5520f7d72ed032f4a2b3b821
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-11 17:55:43 +0200

    commit

---
+++ draggable-components/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main">
+    <board id='bord-01'>
+      <card draggable="true" v-for="card in cardData" :key="card.id" :card='card'/>
+    </board>
+
+     <board id='bord-02' />
+  </div>
+</template>
+
+<script>
+import Board from './components/Board/Board'
+import Card from './components/Card/Card'
+import { cardData } from './data'
+
+export default {
+  data () {
+    return {
+      cardData
+    }
+  },
+  components: {
+    Board,
+    Card
+  }
+}
+</script>
+
+<style lang="scss">
+ * {
+    padding: 0;
+    margin: 0;
+  }
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+
+  .main {
+    padding: 30px;
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    max-width: 900px;
+  }
+</style>
---
+++ draggable-components/src/data.js
@@ -0,0 +1,6 @@
+export const cardData = [
+  { id: 1, text: 'First Line' },
+  { id: 2, text: 'Second Line' },
+  { id: 3, text: 'Third Line' },
+  { id: 4, text: 'Fourth Line' }
+]
