91d72bb8181f0e41b1c5b14ba829bf47a8b42977
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-12 15:15:46 +0200

    commit changes

--- draggable-components/src/App.vue
+++ draggable-components/src/App.vue
@@ -1,27 +1,73 @@
 <template>
   <div class="main">
-    <board id='bord-01'>
-      <card draggable="true" v-for="card in cardData" :key="card.id" :card='card'/>
-    </board>
-
-     <board id='bord-02' />
+      <main-header />
+      <div class="settings">
+        <div class="tasks-input">
+          <input v-if="onEdit" type="text" v-model="tempValue.text" @keyup.enter='updateEdited'>
+          <input v-else type="text" placeholder="New Entry"  v-model="text" @keyup.enter='addTask' />
+          <input v-if="onEdit" type='submit' value='edit' @click="updateEdited"/>
+          <input v-else type='submit' value='submit' @click="addTask"/>
+        </div>
+        <div>
+          <input type="text" placeholder="Create a board" v-model="boardName" @keyup.enter='addNewBoard'>
+          <input type='submit' value='submit'  @click="addNewBoard"/>
+        </div>
+      </div>
+      <board :cardData='cardData'
+             :removeTask='removeTask'
+             :editTask='editTask'
+             :boards='boards'/>
   </div>
 </template>
 
 <script>
-import Board from './components/Board/Board'
-import Card from './components/Card/Card'
 import { cardData } from './data'
+import Board from './Board/Page/BoardPage'
+import MainHeader from './Header/MainHeader'
 
 export default {
   data () {
     return {
-      cardData
+      boards: [],
+      boardName: '',
+      onEdit: false,
+      tempValue: null,
+      cardData,
+      text: ''
     }
   },
   components: {
     Board,
-    Card
+    MainHeader
+  },
+  methods: {
+    addTask (text) {
+      this.cardData.push({ id: Date.now(), text: this.text })
+      this.text = ''
+    },
+    removeTask (id) {
+      this.cardData = this.cardData.filter(e => e.id !== id)
+    },
+    editTask (id) {
+      const item = this.cardData.find(e => e.id === id)
+      this.tempValue = item
+      this.toggleEdit()
+    },
+    toggleEdit () {
+      this.btnLebel = this.btnLebel === 'submit' ? 'edit' : 'submit'
+      this.onEdit = !this.onEdit
+    },
+    updateEdited () {
+      const edited = this.cardData.find(e => e.id === this.tempValue.id)
+      edited.text = this.tempValue.text
+      this.onEdit = false
+    },
+    addNewBoard () {
+      if (this.boardName !== '') {
+        this.boards.push({ id: Date.now(), name: this.boardName })
+        this.boardName = ''
+      }
+    }
   }
 }
 </script>
@@ -38,13 +84,5 @@ export default {
 
   *, *::before, *::after {
     box-sizing: inherit;
-  }
-
-  .main {
-    padding: 30px;
-    display: flex;
-    justify-content: space-evenly;
-    width: 100%;
-    max-width: 900px;
   }
 </style>
